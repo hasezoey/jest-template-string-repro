69e40f5b0f70b5c083344e5a92b2943b3bbdc12f
hasezoey <hasezoey@gmail.com>
2022-05-28 23:57:43 +0200

    update example cases

--- some_source.js
+++ some_source.js
@@ -2,4 +2,6 @@ exports.SomeFunction = function SomeFunction() {
     console.log(`Some Template String ${process.pid}`);
 
     console.log('Some Normal Single-Quote String ' + process.pid);
+
+    console.log(`Some Template String "with some quotes" ${process.pid} and some undefined/null keywords`);
 }
